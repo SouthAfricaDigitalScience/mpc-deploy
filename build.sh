e7b2c202f36148e0a7bf86c3862d9a70a2325b54
Bruce Becker <brucellino@gmail.com>
2015-11-23 00:11:00 +0200

    Added --with-gmp which was missing

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@
 SOURCE_FILE=$NAME-$VERSION.tar.gz
 
 module load ci
+module add gmp
 module add mpfr
+module add ncurses
 module list
 
 
@@ -35,5 +37,5 @@ else
 fi
 cd $WORKSPACE/$NAME-$VERSION
 # We could probably loop through the dependencies here
-./configure --prefix $SOFT_DIR --with-ncurses=$ncurses_DIR --with-mpfr=$MPFR_DIR
+./configure --prefix $SOFT_DIR --with-ncurses=$ncurses_DIR --with-mpfr=$MPFR_DIR  --with-gmp=${GMP_DIR}
 make -j 8
