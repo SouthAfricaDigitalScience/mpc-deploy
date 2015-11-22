b7174e91ad2992dbdd8e803cbc56bbf241d7f623
Bruce Becker <brucellino@gmail.com>
2015-11-23 00:16:53 +0200

    updated for style

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 . /etc/profile.d/modules.sh
-SOURCE_FILE=$NAME-$VERSION.tar.gz
+SOURCE_FILE=${NAME}-${VERSION}.tar.gz
 
 module load ci
 module add gmp
@@ -10,32 +10,32 @@ module list
 
 
 echo "REPO_DIR is "
-echo $REPO_DIR
+echo ${REPO_DIR}
 echo "SRC_DIR is "
-echo $SRC_DIR
+echo ${SRC_DIR}
 echo "WORKSPACE is "
-echo $WORKSPACE
+echo ${WORKSPACE}
 echo "SOFT_DIR is"
-echo $SOFT_DIR
+echo ${SOFT_DIR}
 
-echo $LD_LIBRARY_PATH
+echo ${LD_LIBRARY_PATH}
 
-mkdir -p $WORKSPACE
-mkdir -p $SRC_DIR
-mkdir -p $SOFT_DIR
+mkdir -p ${WORKSPACE}
+mkdir -p ${SRC_DIR}
+mkdir -p ${SOFT_DIR}
 
 #  Download the source file
 
-if [[ ! -s $SRC_DIR/$SOURCE_FILE ]] ; then
+if [[ ! -s ${SRC_DIR}/${SOURCE_FILE} ]] ; then
   echo "seems like this is the first build - let's get the source"
-  mkdir -p $SRC_DIR
-  wget http://mirror.ufs.ac.za/gnu/gnu/$NAME/$SOURCE_FILE -O $SRC_DIR/$SOURCE_FILE
-  tar xvf $SRC_DIR/$SOURCE_FILE -C $WORKSPACE
+  mkdir -p ${SRC_DIR}
+  wget http://mirror.ufs.ac.za/gnu/gnu/${NAME}/${SOURCE_FILE} -O ${SRC_DIR}/${SOURCE_FILE}
+  tar xvf ${SRC_DIR}/${SOURCE_FILE} -C ${WORKSPACE}
 else
-  echo "continuing from previous builds, using source at " $SRC_DIR/$SOURCE_FILE
-  tar xvzf $SRC_DIR/$SOURCE_FILE -C $WORKSPACE
+  echo "continuing from previous builds, using source at " ${SRC_DIR}/${SOURCE_FILE}
+  tar xvzf ${SRC_DIR}/${SOURCE_FILE} -C ${WORKSPACE} --skip-old-files
 fi
-cd $WORKSPACE/$NAME-$VERSION
+cd ${WORKSPACE}/${NAME}-${VERSION}
 # We could probably loop through the dependencies here
-./configure --prefix $SOFT_DIR --with-ncurses=$ncurses_DIR --with-mpfr=$MPFR_DIR  --with-gmp=${GMP_DIR}
+./configure --prefix ${SOFT_DIR} --with-ncurses=${ncurses_DIR} --with-mpfr=${MPFR_DIR}  --with-gmp=${GMP_DIR}
 make -j 8
--- check-build.sh
+++ check-build.sh
@@ -2,15 +2,15 @@
 . /etc/profile.d/modules.sh
 module add ci
 module add mpfr
-cd $WORKSPACE/$NAME-$VERSION
+cd ${WORKSPACE}/${NAME}-${VERSION}
 
 make check
 
 make install # this will install to /apprepo
-make install DESTDIR=$WORKSPACE/build # this will create a redistributable tarball
-mkdir -p $REPO_DIR
-rm -rf $REPO_DIR/*
-tar -cvzf $REPO_DIR/build.tar.gz -C $WORKSPACE/build apprepo
+make install DESTDIR=${WORKSPACE}/build # this will create a redistributable tarball
+mkdir -p ${REPO_DIR}
+rm -rf ${REPO_DIR}/*
+tar -cvzf ${REPO_DIR}/build.tar.gz -C ${WORKSPACE}/build apprepo
 mkdir -p modules
 (
 cat <<MODULE_FILE
@@ -29,6 +29,6 @@ setenv MPC_DIR /apprepo/$::env(SITE)/$::env(OS)/$::env(ARCH)/$NAME/$VERSION
 prepend-path LD_LIBRARY_PATH $::env(MPC_DIR)/lib
 prepend-path GCC_INCLUDE_DIR $::env(MPC_DIR)/lib
 MODULE_FILE
-) > modules/$VERSION
-mkdir -p $LIBRARIES_MODULES/$NAME
-cp modules/$VERSION $LIBRARIES_MODULES/$NAME
+) > modules/${VERSION}
+mkdir -p ${LIBRARIES_MODULES}/${NAME}
+cp modules/${VERSION} ${LIBRARIES_MODULES}/${NAME}
