09dac55f54ccdaee06293038541671dbd335b31c
Bruce Becker <brucellino@gmail.com>
2018-08-21 17:50:42 +0200

    change download url for tarball

--- build.sh
+++ build.sh
@@ -43,7 +43,7 @@ if [ ! -e ${SRC_DIR}/${SOURCE_FILE}.lock ] && [ ! -s ${SRC_DIR}/${SOURCE_FILE} ]
   touch  ${SRC_DIR}/${SOURCE_FILE}.lock
   echo "seems like this is the first build - let's get the source"
   mkdir -p ${SRC_DIR}
-  wget http://mirror.ufs.ac.za/gnu/gnu/${NAME}/${SOURCE_FILE} -O ${SRC_DIR}/${SOURCE_FILE}
+  wget http://mirror.ufs.ac.za/gnu/${NAME}/${SOURCE_FILE} -O ${SRC_DIR}/${SOURCE_FILE}
   echo "releasing lock"
   rm -v ${SRC_DIR}/${SOURCE_FILE}.lock
   elif [ -e ${SRC_DIR}/${SOURCE_FILE}.lock ] ; then
